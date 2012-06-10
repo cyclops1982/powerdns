880090560f5137c42958cc9ad9d2483d5ba3c89e
Ruben d'Arco <cyclops@prof-x.net>
2012-06-10 09:51:10 +0200

    More tests!

---
+++ regression-tests/dyndns-update-add-known-host/sendupdate.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use Net::DNS;
+use Net::DNS::Update;
+
+my $update = Net::DNS::Update->new('test.dyndns');
+$update->push(update => rr_add('host-3.test.dyndns. 120 A 127.0.0.125'));
+
+my $res = Net::DNS::Resolver->new;
+$res->nameservers($ARGV[0]);
+$res->port($ARGV[1]);
+ 
+my $reply = $res->send($update);
+ 
+if ($reply) {
+	print "RCODE: ", $reply->header->rcode, "\n";
+} else {
+	print "ERROR: ", $res->errorstring, "\n";
+}
