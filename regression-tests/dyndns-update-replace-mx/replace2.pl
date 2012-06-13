49b60bd1ce91d10fd1077f8a62bde4c5a6f22c09
Ruben d'Arco <cyclops@prof-x.net>
2012-06-13 23:06:48 +0200

    Add replace for MX record.

---
+++ regression-tests/dyndns-update-replace-mx/replace2.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use strict;
+use Net::DNS;
+use Net::DNS::Update;
+
+my $update = Net::DNS::Update->new('test.dyndns');
+$update->push(update => rr_add('test.dyndns. 3600 MX 10 host-1.test.dyndns'));
+
+my $res = Net::DNS::Resolver->new;
+$res->nameservers($ARGV[0]);
+$res->port($ARGV[1]);
+ 
+my $reply = $res->send($update);
+ 
+if ($reply) {
+	print "RCODE: ", $reply->header->rcode, "\n";
+} else {
+	print "ERROR: ", $res->errorstring, "\n";
+}
